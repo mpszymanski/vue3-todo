5818ee48e9bcf1c28ade7bc730204a4ffa4b4ad1
Michał Szymański <m.szymanski@smartersite.pl>
2021-01-10 19:24:11 +0100

    add font awesome support

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Tailwind styles
 import './index.css'
 
-createApp(App).mount('#app')
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const app = createApp(App)
+
+library.add(faCheckCircle)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.config.productionTip = false
+
+app.mount('#app')
