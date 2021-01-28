64566015dd7d1d1ce3b78942f74795ccdf8f1460
Michał Szymański <m.szymanski@smartersite.pl>
2021-01-28 22:35:02 +0100

    add PWA support

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue3-todo/" : "/"
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/vue3-todo/" : "/",
+  pwa: {
+    name: "To do",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+  }
+};
