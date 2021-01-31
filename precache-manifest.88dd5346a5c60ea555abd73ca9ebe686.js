891803b543363423f8e705ab236487af95c40e93
Michał Szymański <m.szymanski@smartersite.pl>
2021-01-31 17:33:40 +0100

    deploy

---
+++ precache-manifest.88dd5346a5c60ea555abd73ca9ebe686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7373b430f65ad34466",
+    "url": "/vue3-todo/css/app.5ef4fdeb.css"
+  },
+  {
+    "revision": "a7c3aaaf22abdee4b2cad979053595ea",
+    "url": "/vue3-todo/index.html"
+  },
+  {
+    "revision": "6a7373b430f65ad34466",
+    "url": "/vue3-todo/js/app.7838900c.js"
+  },
+  {
+    "revision": "2f42cf796f22adb832ed",
+    "url": "/vue3-todo/js/chunk-vendors.6516227f.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/vue3-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-todo/robots.txt"
+  }
+]);
